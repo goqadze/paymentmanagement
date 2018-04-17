2d9f4c2084152cbec100dbe63dd5e79ae2dd315a
goqadze <agoqadze@gmail.com>
2018-04-18 00:09:43 +0400

    refactor

--- public/js/store/api/index.js
+++ public/js/store/api/index.js
@@ -1,13 +1,13 @@
 const queryParams = (params) => {
-  return Object.keys(params)
-    .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
-    .join('&');
-}
+    return Object.keys(params)
+        .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
+        .join('&');
+};
 
 
 export const fetchPayments = (filter) => {
-  const url = '/filter?' + queryParams(filter);
-  return fetch(url).then(res => res.json());
+    const url = '/filter?' + queryParams(filter);
+    return fetch(url).then(res => res.json());
 };
 
 export const addPayment = (newPayment) => {
